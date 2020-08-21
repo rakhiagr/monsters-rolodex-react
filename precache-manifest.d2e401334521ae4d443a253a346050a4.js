6fe393e18849dd0eed3e9c3478ca8b3b7afc9093
rakhiagr <rakhiagr2020@gmail.com>
2020-08-21 13:29:35 +0530

    Updates

---
+++ precache-manifest.d2e401334521ae4d443a253a346050a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c039e16c89fd0880fd5431c666084ac4",
+    "url": "/monsters-rolodex-react/index.html"
+  },
+  {
+    "revision": "54816aa344820d052a7f",
+    "url": "/monsters-rolodex-react/static/css/main.3d80aeb6.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex-react/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-react/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54816aa344820d052a7f",
+    "url": "/monsters-rolodex-react/static/js/main.f41eae49.chunk.js"
+  },
+  {
+    "revision": "ab8efdbffd13bd88af79",
+    "url": "/monsters-rolodex-react/static/js/runtime-main.524445f2.js"
+  }
+]);
